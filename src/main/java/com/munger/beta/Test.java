b87e4c234000c47441bcfe372c28a7bf5b187b30
mechanicI <w642902725@gmail.com>
2023-03-31 11:45:13 +0800

    first commit

---
+++ src/main/java/com/munger/beta/Test.java
@@ -0,0 +1,16 @@
+package com.munger.beta;
+
+/**
+ * @Author: Munger.;
+ * @Date: 2023/3/31 11:33;
+ * @Params:
+ * @Return:
+ * @Description:
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("munger spring");
+    }
+
+}
